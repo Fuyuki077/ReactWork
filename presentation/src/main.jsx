ad2ef0491fc25b2ef59cb4b6550ba137d2e5d323
CristianoSP <cristianos.pontes@hotmail.com>
2025-11-26 11:11:19 -0400

    troca do pacote home pelo pacote do react bits navbarcards

--- presentation/src/main.jsx
+++ presentation/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home'
+import Menu from './pages/Home/headerer/headerer.jsx'
+// import CardNav from './pages/Home/headerer/CardNav.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Menu />
+    {/* <Home /> */}
   </StrictMode>,
 )
